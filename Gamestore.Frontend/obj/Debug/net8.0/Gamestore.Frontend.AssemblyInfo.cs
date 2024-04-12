3ee148c71ea4b23774409991a92f4193a12050ed
Aditya <jusadyee@gmail.com>
2024-04-13 00:09:21 +0545

    Update Gamestore.Frontend cache and assembly info files

--- Gamestore.Frontend/obj/Debug/net8.0/Gamestore.Frontend.AssemblyInfo.cs
+++ Gamestore.Frontend/obj/Debug/net8.0/Gamestore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gamestore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0209d8db82452dcfb9d096bfe25bf9e86cfc1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gamestore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gamestore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
