cf2e674e5a3f5b44d7b0ca29a6dbe78058ae345a
Aditya <jusadyee@gmail.com>
2024-04-17 00:08:49 +0545

    Update GameSummary model in Gamestore.Frontend

--- Gamestore.Frontend/Models/GameSummary.cs
+++ Gamestore.Frontend/Models/GameSummary.cs
@@ -2,5 +2,9 @@
 
 public class GameSummary
 {
-    
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Genre { get; set; }
+    public decimal Price { get; set; }
+    public DateOnly ReleaseDate { get; set; } 
 }
