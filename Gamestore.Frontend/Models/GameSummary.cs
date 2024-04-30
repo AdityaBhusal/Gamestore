35a4bbedb922b45c75c7ec5accdcbf548e298dbc
Aditya <jusadyee@gmail.com>
2024-04-30 16:03:02 +0545

    Update Gamestore.Frontend home page with new game entries and fix formatting

--- Gamestore.Frontend/Models/GameSummary.cs
+++ Gamestore.Frontend/Models/GameSummary.cs
@@ -5,6 +5,7 @@ public class GameSummary
     public int Id { get; set; }
     public required string Name { get; set; }
     public required string Genre { get; set; }
+    public DateOnly ReleaseDate { get; set; }
     public decimal Price { get; set; }
-    public DateOnly ReleaseDate { get; set; } 
+
 }
