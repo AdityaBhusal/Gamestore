846d07a3fa0ef8eea9d7d7238caa29a00213f933
Aditya <jusadyee@gmail.com>
2024-05-21 23:17:24 +0545

    Update `EditGame.razor` to inject `GamesClient` and remove cancel button

--- Gamestore.Frontend/Program.cs
+++ Gamestore.Frontend/Program.cs
@@ -1,10 +1,11 @@
+using Gamestore.Frontend.Clients;
 using Gamestore.Frontend.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents();
-
+builder.Services.AddSingleton<GamesClient>();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
