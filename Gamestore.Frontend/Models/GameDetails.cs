deb81666ce760b4389839f413ca64c4a18ee9026
Aditya <jusadyee@gmail.com>
2024-05-22 22:10:52 +0545

    feat: Add validation attributes to GameDetails model
    This commit adds validation attributes to the `GameDetails` model. The `Name` property is now required and must be a string with a maximum length of 50 characters. The `ReleaseDate` property is also required and must be a date between 1 and 100.
    
    These changes ensure that the `GameDetails` model enforces the necessary validation rules, improving data integrity and preventing invalid data from being submitted.

--- Gamestore.Frontend/Models/GameDetails.cs
+++ Gamestore.Frontend/Models/GameDetails.cs
@@ -1,10 +1,16 @@
-﻿namespace Gamestore.Frontend.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gamestore.Frontend.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string  Name { get; set; }
+    [Required]
     public string? GenreId { get; set; }
+    [Range(1,100)]
     public DateOnly ReleaseDate { get; set; }
     public decimal Price { get; set; }
 
