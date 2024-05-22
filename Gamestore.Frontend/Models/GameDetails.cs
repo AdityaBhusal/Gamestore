7c99430283c2bd1bdc3de53db66527a74701bdf6
Aditya <jusadyee@gmail.com>
2024-05-22 22:26:55 +0545

    feat: Add validation summary to EditGame page
    This commit adds a validation summary component to the EditGame page. The validation summary displays any validation errors that occur when submitting the form. This provides users with a clear indication of any invalid input and helps them correct their mistakes.
    
    The validation summary is added to the EditGame.razor file, inside the EditForm component. It ensures that users are aware of any errors and can take appropriate action.
    
    This feature enhances the user experience by improving the visibility of validation errors and guiding users towards submitting valid data.

--- Gamestore.Frontend/Models/GameDetails.cs
+++ Gamestore.Frontend/Models/GameDetails.cs
@@ -8,7 +8,7 @@ public class GameDetails
     [Required]
     [StringLength(50)]
     public required string  Name { get; set; }
-    [Required]
+    [Required(ErrorMessage = "The Genre field is required.")]
     public string? GenreId { get; set; }
     public DateOnly ReleaseDate { get; set; }
     [Range(1,100)]
