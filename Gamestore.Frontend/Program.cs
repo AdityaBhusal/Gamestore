71ba9e7a7c3b1ec7e00abded09657033b58d6329
Aditya <jusadyee@gmail.com>
2024-06-11 23:00:01 +0545

    feat: Add InteractiveServer render mode to DeleteGame.razor and Program.cs
    This commit adds the InteractiveServer render mode to the DeleteGame.razor and Program.cs files. By using the InteractiveServer render mode, the application can provide a more interactive and responsive user experience. This enhancement improves the performance and usability of the Delete Game page and the overall application.

--- Gamestore.Frontend/Program.cs
+++ Gamestore.Frontend/Program.cs
@@ -4,7 +4,8 @@ using Gamestore.Frontend.Components;
 var builder = WebApplication.CreateBuilder(args);
 // comment
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents().
+            AddInteractiveServerComponents();
 builder.Services.AddSingleton<GamesClient>();
 builder.Services.AddSingleton<GenresClient>();
 
@@ -22,6 +23,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
