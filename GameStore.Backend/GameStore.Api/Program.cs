8546d785488877f93c18390f04b24481d7ec19fc
Aditya <jusadyee@gmail.com>
2024-06-14 10:59:47 +0545

    feat: Add initial game data to Program.cs
    This commit adds initial game data to the Program.cs file. The data includes three game objects with their respective properties such as ID, title, genre, price, and release date. This enhancement improves the functionality of the application by providing pre-defined game data for testing and demonstration purposes.

--- GameStore.Backend/GameStore.Api/Program.cs
+++ GameStore.Backend/GameStore.Api/Program.cs
@@ -1,7 +1,31 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-
+List<GameDto> games=[
+    new(
+        1,
+        "Super Mario Bros",
+        "Platform",
+        59.99m,
+        new DateOnly(1985, 9, 13)
+    ),
+    new(
+        2,
+        "The Legend of Zelda",
+        "Action-Adventure",
+        49.99m,
+        new DateOnly(1986, 2, 21)
+    ),
+    new(
+        3,
+        "Minecraft",
+        "Sandbox",
+        29.99m,
+        new DateOnly(2011, 11, 18)
+    )
+]
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
