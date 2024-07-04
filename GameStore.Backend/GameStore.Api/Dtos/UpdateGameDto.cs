6278608443a2eaffcc8b818d039e13c63bf951c0
Aditya <jusadyee@gmail.com>
2024-07-04 10:28:54 +0545

    feat: Update GameStore.Api.AssemblyInfo.cs version to 1.0.0

---
+++ GameStore.Backend/GameStore.Api/Dtos/UpdateGameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStore.Api.Dtos;
+
+public record class UpdateGameDto(
+     string Name,
+      string Genre,
+       decimal Price,
+       DateOnly ReleaseDate 
+);
--- GameStore.Backend/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Backend/GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925b32a561ac92e324f309d308c29e3003bd43bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
