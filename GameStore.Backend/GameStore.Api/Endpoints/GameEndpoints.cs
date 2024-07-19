4b5685a8cbc78d2bed01c38df2f99f4470b14118
Aditya <jusadyee@gmail.com>
2024-07-20 00:02:23 +0545

    fix: Update Delete Endpoint made it work
    if game is not found -404 not found
    if game is found and deleted - 204 no content

--- GameStore.Backend/GameStore.Api/Endpoints/GameEndpoints.cs
+++ GameStore.Backend/GameStore.Api/Endpoints/GameEndpoints.cs
@@ -5,95 +5,89 @@ namespace GameStore.Api.Endpoints;
 
 public static class GameEndpoints
 {
-const string GetGameEndPointName = "GetGame";
-    private static readonly List<GameDto> games=[
-    new(
-        1,
-        "Super Mario Bros",
-        "Platform",
-        59.99m,
-        new DateOnly(1985, 9, 13)
-    ),
-   
-    new(
-        2,
-        "The Legend of Zelda",
-        "Action-Adventure",
-        49.99m,
+    const string GetGameEndPointName = "GetGame";
+    private static readonly List<GameDto> games =
+    [
+        new(1, "Super Mario Bros", "Platform", 59.99m, new DateOnly(1985, 9, 13)),
+        new(2, "The Legend of Zelda", "Action-Adventure", 49.99m, new DateOnly(1986, 2, 21)),
+        new(3, "Minecraft", "Sandbox", 29.99m, new DateOnly(2011, 11, 18))
+    ];
 
-        new DateOnly(1986, 2, 21)
-    ),
-    new(
-        3,
-        "Minecraft",
-        "Sandbox",
-        29.99m,
-        new DateOnly(2011, 11, 18)
-    )
-];
-
-public static WebApplication MapGamesEndpoints (this WebApplication app){
-    
-//GET /games
-app.MapGet("games", () => games);
-
-//Get /games/{id}
-app.MapGet("games/{id}",
- (int id)=>
-{
-    GameDto? game =  games.Find(g=>g.Id==id);
-
-    return game is not null ? Results.Ok(game): Results.NotFound();
-
-})
-.WithName(GetGameEndPointName);
-
- //post /games
- app.MapPost("games", (CreateGameDto newGame) =>
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
     {
-        GameDto game = new(
-            games.Count + 1,
-            newGame.Name,
-            newGame.Genre,
-            newGame.Price,
-            newGame.ReleaseDate);
+        var group = app.MapGroup("games");
 
-        games.Add(game);
+        //GET /games
+        group.MapGet("", () => games);
 
-        return Results.CreatedAtRoute(GetGameEndPointName, new {id = game.Id}, game);
-    });
+        //Get /games/{id}
+        group
+            .MapGet(
+                "/{id}",
+                (int id) =>
+                {
+                    GameDto? game = games.Find(g => g.Id == id);
 
-//put /games/{id}   
-app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
-{
-    var index = games.FindIndex(game => game.Id == id );
+                    return game is not null ? Results.Ok(game) : Results.NotFound();
+                }
+            )
+            .WithName(GetGameEndPointName);
 
-    if(index == -1)
-    {
-        return Results.NotFound();
+        //post /games
+        group.MapPost(
+            "",
+            (CreateGameDto newGame) =>
+            {
+                GameDto game =
+                    new(
+                        games.Count + 1,
+                        newGame.Name,
+                        newGame.Genre,
+                        newGame.Price,
+                        newGame.ReleaseDate
+                    );
+
+                games.Add(game);
+
+                return Results.CreatedAtRoute(GetGameEndPointName, new { id = game.Id }, game);
+            }
+        );
+
+        //put /games/{id}
+        group.MapPut(
+            "/{id}",
+            (int id, UpdateGameDto updatedGame) =>
+            {
+                var index = games.FindIndex(game => game.Id == id);
+
+                if (index == -1)
+                {
+                    return Results.NotFound();
+                }
+
+                games[index] = new GameDto(
+                    id,
+                    updatedGame.Name,
+                    updatedGame.Genre,
+                    updatedGame.Price,
+                    updatedGame.ReleaseDate
+                );
+
+                return Results.NoContent();
+            }
+        );
+
+        //delete /game/{id}
+        group.MapDelete("/{id}", (int id) =>
+        {
+            var game = games.RemoveAll(g => g.Id == id);
+
+
+            //If the game was deleted, return 204 No Content
+            //If the game was not found, return 404 Not Found
+            return game > 0 ? Results.NoContent() : Results.NotFound();
+        });
+
+        return group;
     }
-
-    games[index] = new GameDto
-    (
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
-
-//delete /game/{id}
-app.MapDelete("games/{id}",(int id)=>
-{
-    games.RemoveAll(game=>game.Id == id);
-    return Results.NoContent;
-}
-);
-return app;
-}
-
-
 }
