3ef273c7fc7db471f5ac28b2c1703cc6ba921e59
Aditya <jusadyee@gmail.com>
2024-07-20 23:42:21 +0545

    feat: Add validation attributes to CreateGameDto properties

--- GameStore.Backend/GameStore.Api/Dtos/CreateGameDto.cs
+++ GameStore.Backend/GameStore.Api/Dtos/CreateGameDto.cs
@@ -1,9 +1,11 @@
-﻿namespace GameStore.Api.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Dtos;
 
 public record class CreateGameDto
 (
-     string Name,
-      string Genre,
-       decimal Price,
-       DateOnly ReleaseDate 
+     [Required][StringLength(50)]string Name,
+     [Required][StringLength(20)]string Genre,
+     [Range(1,100)]decimal Price,
+     DateOnly ReleaseDate 
 );
