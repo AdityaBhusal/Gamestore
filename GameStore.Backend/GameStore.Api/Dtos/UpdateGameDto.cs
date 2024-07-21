37b397e61a207c4f35d79061cd337ad039c5a86b
Aditya <jusadyee@gmail.com>
2024-07-21 22:43:52 +0545

    refactor: UpdateGameDto class - align properties and remove whitespace
    This commit refactors the UpdateGameDto class in the UpdateGameDto.cs file. It improves the formatting and readability of the code by aligning the properties and removing unnecessary whitespace. This enhancement enhances the maintainability and consistency of the codebase.

--- GameStore.Backend/GameStore.Api/Dtos/UpdateGameDto.cs
+++ GameStore.Backend/GameStore.Api/Dtos/UpdateGameDto.cs
@@ -1,8 +1,10 @@
-﻿namespace GameStore.Api.Dtos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Dtos;
 
 public record class UpdateGameDto(
-     string Name,
-     string Genre,
-     decimal Price,
+     [Required][StringLength(50)]string Name,
+     [Required][StringLength(20)]string Genre,
+     [Range(1,100)]decimal Price,
      DateOnly ReleaseDate 
 );
