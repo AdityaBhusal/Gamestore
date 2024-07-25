544161c714da0ef1e05c6eb6b3cec4a14a580cc1
Aditya <jusadyee@gmail.com>
2024-07-25 22:21:58 +0545

    refactor: Remove unused GameContext class and add GameStoreContext class

--- GameStore.Backend/GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Backend/GameStore.Api/Data/GameStoreContext.cs
@@ -6,5 +6,7 @@ namespace GameStore.Api.Data;
 public class GameStoreContext (DbContextOptions<GameStoreContext> options)
  : DbContext(options)
 {
-    public DbSet<Game> Games { get; set; };
+    public DbSet<Game> Games => Set<Game>();
+
+    public DbSet<Genre> Genre => Set<Genre>();
 }
