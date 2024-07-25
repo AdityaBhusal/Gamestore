9670670a8be74de89f2d5ab2ff97f703bf651e16
Aditya <jusadyee@gmail.com>
2024-07-25 22:28:24 +0545

    feat: Add connstring var to add the path to the database for the gamestorecontext.cs

--- GameStore.Backend/GameStore.Api/Program.cs
+++ GameStore.Backend/GameStore.Api/Program.cs
@@ -1,6 +1,10 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connString =  "DataSource=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
 
