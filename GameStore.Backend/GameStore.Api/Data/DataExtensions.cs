b0829357aec897ce1dee2cb7eb80f5dc6a028ba9
Aditya <jusadyee@gmail.com>
2024-07-27 21:12:19 +0545

    feat: Add GameStoreContext class for managing database interactions

---
+++ GameStore.Backend/GameStore.Api/Data/DataExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public static class DataExtensions
+{
+    public static void MigrateDb(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        dbContext.Database.Migrate();
+    }
+}
