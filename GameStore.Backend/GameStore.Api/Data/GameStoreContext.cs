e15ef081a7da4ee6066ecc6b14a068167e2afdf4
Aditya <jusadyee@gmail.com>
2024-07-28 23:16:11 +0545

    refactor: Remove unused GameContext class and update GameStoreContext seeding
    - Remove unused GameContext class to improve code organization and clarity.
    - Update GameStoreContext seeding data to include new game genres:
      - Mahjong
      - Solitaire
      - Casino
      - Arcade
      - Pinball
      - Beat 'em Up
      - Hack and Slash

--- GameStore.Backend/GameStore.Api/Data/GameStoreContext.cs
+++ GameStore.Backend/GameStore.Api/Data/GameStoreContext.cs
@@ -5,7 +5,8 @@ namespace GameStore.Api.Data;
 
 public class GameStoreContext : DbContext
 {
-    public GameStoreContext(DbContextOptions<GameStoreContext> options) : base(options) { }
+    public GameStoreContext(DbContextOptions<GameStoreContext> options)
+        : base(options) { }
 
     public DbSet<Game> Games => Set<Game>();
     public DbSet<Genre> Genre => Set<Genre>();
