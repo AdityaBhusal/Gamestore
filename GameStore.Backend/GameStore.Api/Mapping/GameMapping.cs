a11f7907366d1512f09e38eb87bd8ff16d427b77
Aditya <jusadyee@gmail.com>
2024-07-31 23:14:58 +0545

    refactor: Add GameMapping class for mapping between CreateGameDto and Game entities

---
+++ GameStore.Backend/GameStore.Api/Mapping/GameMapping.cs
@@ -0,0 +1,23 @@
+﻿using GameStore.Api.Dtos;
+using GameStore.Api.Entities;
+
+namespace GameStore.Api.Mapping;
+
+public static class GameMapping
+{
+    public static Game ToEntity(this CreateGameDto game)
+    {
+        return new Game()
+        {
+            Name = game.Name,
+            GenreId = game.GenreId,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+    }
+
+    public static GameDto ToDto(this Game game)
+    {
+        return new(game.Id, game.Name, game.Genre!.Name, game.Price, game.ReleaseDate);
+    }
+}
