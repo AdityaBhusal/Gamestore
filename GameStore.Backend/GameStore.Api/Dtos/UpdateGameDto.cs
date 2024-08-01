cad70626299bd6f900a30eb2a5400d31a0c16527
Aditya <jusadyee@gmail.com>
2024-08-01 22:20:49 +0545

    refactor: Update GameEndpoints.cs to use GameMapping for entity mapping

--- GameStore.Backend/GameStore.Api/Dtos/UpdateGameDto.cs
+++ GameStore.Backend/GameStore.Api/Dtos/UpdateGameDto.cs
@@ -3,8 +3,8 @@
 namespace GameStore.Api.Dtos;
 
 public record class UpdateGameDto(
-     [Required][StringLength(50)]string Name,
-     [Required][StringLength(20)]string Genre,
-     [Range(1,100)]decimal Price,
-     DateOnly ReleaseDate 
+    [Required] [StringLength(50)] string Name,
+    int GenreId,
+    [Range(1, 100)] decimal Price,
+    DateOnly ReleaseDate
 );
