04a8d035dc9e4b54d09881c749de75da7b9a60d1
Aditya <jusadyee@gmail.com>
2024-08-01 21:50:51 +0545

    refactor: Update GameMapping.cs to include ToGameDetailsDto method
    - Add ToGameDetailsDto method to GameMapping.cs for mapping Game entities to GameDetailsDto.

--- GameStore.Backend/GameStore.Api/Mapping/GameMapping.cs
+++ GameStore.Backend/GameStore.Api/Mapping/GameMapping.cs
@@ -16,8 +16,13 @@ public static class GameMapping
         };
     }
 
-    public static GameDto ToDto(this Game game)
+    public static GameSummaryDto ToGameSummaryDto(this Game game)
     {
         return new(game.Id, game.Name, game.Genre!.Name, game.Price, game.ReleaseDate);
+    }
+
+    public static GameDetailsDto ToGameDetailsDto(this Game game)
+    {
+        return new(game.Id, game.Name, game.GenreId, game.Price, game.ReleaseDate);
     }
 }
